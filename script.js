301714a6902c566f8eb3874ce759d40c9946f2d3
Илья Хамраев <ikhamraev@MacBook-Pro-Ila.local>
2023-02-10 16:14:35 +0500

    feat(): added form create bookmarks

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("started");
+const configClasses = {
+  hidden: "hidden",
+};
+
+const initialBookmarks = [];
+
+const btnShow = document.querySelector(".button_show");
+const createForm = document.querySelector(".create-form");
+
+const toggleShowHide = (node) => {
+  const classList = node.classList;
+  const arrayClassList = Array.from(classList);
+
+  if (arrayClassList.includes(configClasses.hidden)) {
+    classList.remove(configClasses.hidden);
+    return;
+  }
+  classList.add(configClasses.hidden);
+};
+
+btnShow.addEventListener("click", () => toggleShowHide(createForm));
